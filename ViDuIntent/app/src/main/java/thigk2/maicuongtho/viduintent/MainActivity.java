24a4dff4308c7866050b7487124832bc1ba47750
Mai Cường Thọ <thomc@ntu.edu.vn>
2025-10-22 09:41:14 +0700

    Code hoan thien man hinh chinh

--- ViDuIntent/app/src/main/java/thigk2/maicuongtho/viduintent/MainActivity.java
+++ ViDuIntent/app/src/main/java/thigk2/maicuongtho/viduintent/MainActivity.java
@@ -1,6 +1,10 @@
 package thigk2.maicuongtho.viduintent;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +18,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       Button nutChuyen2 =  findViewById(R.id.btnChuyenSang2);
+       nutChuyen2.setOnClickListener(new View.OnClickListener() {
+           @Override
+           public void onClick(View v) {
+               // Code chuyen man hinh
+               // 1. Tao intnet
+               Intent iChuyen2 = new Intent(MainActivity.this,SecondActivity.class);
+               // 2. Goi du lieu vao iChuyen2
+                    //2.1 lay du lieu
+                     EditText edHoTen= findViewById(R.id.edHoTen);
+                     String data=edHoTen.getText().toString();
+                    //2.2. Goi vao iChuyen2, dung putExtra(key, value)
+                    iChuyen2.putExtra("ht",data);
+                    iChuyen2.putExtra("copyright","MCT"); // just fun/trap
+
+               //3 Kich hoat man hinh 2
+               startActivity(iChuyen2);
+           }
+       });
 
     }
 }
